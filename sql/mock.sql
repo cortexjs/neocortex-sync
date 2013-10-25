131eb24ab26dfaacfd673a5062a1af999b5ba681
Wei Gao <jky239@gmail.com>
2013-10-25 15:54:48 +0800

    update mock.sql, add formatted mock data

--- sql/mock.sql
+++ sql/mock.sql
@@ -4,7 +4,7 @@ TRUNCATE TABLE CM_CortexCombo;
 INSERT INTO CM_CortexDependencies  (Name, Version, Dependencies, Csses) 
     VALUES ('type', '1.1.0', '', ''), ('type', '1.1.1', '', ''), ('type', '1.1.2', '', ''), 
     ('indexof', '0.3.0', '', ''), ('indexof', '0.3.1', '', ''), ('jsonp', '0.0.1', 'type@~1.1.0,indexof@0.3.0', ''),
-    ('combobox', '0.0.1', 'jsonp@0.0.1,type@1.1.1', 'combobox'),
+    ('combobox', '0.0.1', 'jsonp@0.0.1,type@~1.1.0,indexof@0.3.0,type@1.1.1', 'combobox'),
     ('mbox', '0.1.0', '', 'mbox'), ('tablet', '0.0.1', 'mbox@~0.1.0', 'tablet');
 
 INSERT INTO CM_CortexCombo (Name, Version, ComboId)
