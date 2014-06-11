a9affb9bdd675c9c91e145d05c05cb28476ced6f
supersheep <supersheepxjy@gmail.com>
2014-06-11 17:22:48 +0800

    support css

---
+++ nightly-build.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var spawns = require('spawns');
+var jf = require('jsonfile');
+
+var pkg = jf.readFileSync('package.json');
+
+function changeName(name, old) {
+  pkg.name = name;
+  pkg.bin[name] = pkg.bin[old];
+  delete pkg.bin[old];
+  jf.writeFileSync('package.json', pkg);
+  console.log('name changed to', name);
+}
+
+process.on('exit', function() {
+  changeName('neocortex-sync', 'neocortex-sync-canary');
+});
+
+changeName('neocortex-sync-canary', 'neocortex-sync');
+
+spawns('npm publish', {
+  cwd: __dirname,
+  stdio: 'inherit'
+}).on('spawn', function(command) {
+  console.log(command);
+}).on('close', function(code) {
+  console.log('exit code', code);
+
+}).on('error', function(err) {
+  console.log('err', err);
+});
--- sql/addcolumn.sql
+++ sql/addcolumn.sql
@@ -1,2 +1,3 @@
 alter table `CM_CortexPackageDependencies` add COLUMN `Size` int(11) COMMENT 'Gzipped size from compressed main file';
 alter table `CM_CortexPackageDependencies` add COLUMN `AsyncDependencies` text;
+alter table `CM_CortexPackageDependencies` add COLUMN `NoJS` tinyint(1);
